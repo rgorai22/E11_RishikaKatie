d10080d2394308a4b4374ef15bcf947d9ced07ff
rgorai22 <158365742+rgorai22@users.noreply.github.com>
2024-02-07 15:27:39 -0800

    Update airquality.py uncomments + comments

--- airquality.py
+++ airquality.py
@@ -10,7 +10,7 @@ import time
 import board
 import busio
 from digitalio import DigitalInOut, Direction, Pull
-from adafruit_pm25.i2c import PM25_I2C
+#from adafruit_pm25.i2c import PM25_I2C
 
 
 reset_pin = None
@@ -33,17 +33,17 @@ import serial
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
 
 # For use with USB-to-serial cable:
-import serial
-uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=0.25)
+#import serial
+#uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=0.25)
 
 # Connect to a PM2.5 sensor over UART
-# from adafruit_pm25.uart import PM25_UART
-# pm25 = PM25_UART(uart, reset_pin)
+from adafruit_pm25.uart import PM25_UART
+pm25 = PM25_UART(uart, reset_pin)
 
 # Create library object, use 'slow' 100KHz frequency!
 #i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
 # Connect to a PM2.5 sensor over I2C
-pm25 = PM25_I2C(i2c, reset_pin)
+#pm25 = PM25_I2C(i2c, reset_pin)
 
 print("Found PM2.5 sensor, reading data...")
 
